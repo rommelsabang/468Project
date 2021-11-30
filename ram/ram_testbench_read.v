e0b11677a456bd7b35137360f09230bfc330382c
rommelsabang <93158433+rommelsabang@users.noreply.github.com>
2021-11-30 15:49:51 -0800

    Update ram_testbench_read.v

--- ram/ram_testbench_read.v
+++ ram/ram_testbench_read.v
@@ -1,28 +1,31 @@
 module ram_testbench_read;
 
-reg CLK,RW,ENABLE,F;
+reg CLK,ENABLE;
+reg [1:0] RW;
 reg [31:0] DIN;
 reg [31:0] ADDR;
 wire [31:0] DOUT,FETCH;
 
-ram RAM_12(ADDR,RW,DIN,DOUT,FETCH,F,CLK,ENABLE); 
+ram RAM_12(ADDR,RW,DIN,DOUT,FETCH,CLK,ENABLE); 
 
 initial
 begin  
 
-  $readmemh("E:/ENGR 4th year/ENGR 468/project_vs/data_h.txt", RAM_12.ram);
+  $readmemb("E:/ENGR 4th year/ENGR 468/test documents from canvas/LDR Instruction Set v2.txt", RAM_12.ram);
+
 
 	CLK = 0;
-   ENABLE =0;   RW=0;	ADDR=32'd0; F = 1; 	
-#5 ENABLE =1;   RW=1;	ADDR=32'd0;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd1;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd2;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd3;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd4;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd5;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd6;	
-#5 ENABLE =1;   RW=1;	ADDR=32'd7;	
+   ENABLE =0;   RW=2'b0;	ADDR=32'd0;  	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd0;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd1;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd2;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd3;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd4;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd5;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd6;	
+#5 ENABLE =1;   RW=2'b01;	ADDR=32'd7;	
 
+#50 $finish; 
 end
 
 
@@ -34,4 +37,3 @@ end
 always #5 CLK=~CLK;
 
 endmodule
-
